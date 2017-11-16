53b3d25b14250033b43cc4dcc134b880e89d8a6f
Denis Chernov <denis.chernov@inspiretec.com>
2017-11-16 11:45:54 +0200

    Solution structure added.

---
+++ DigitalLeap/DigitalLeap.Web/Controllers/FeaturesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace DigitalLeap.Web.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        // GET: Home
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult Capabilities()
+        {
+            return View();
+        }
+        public ActionResult ChoosePlan()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
